a5650956b57d7fc6bb7ac96b3968302e7c937329
Eric Loh <lygao1995@gmail.com>
2017-07-31 20:13:12 +0800

    added imageurl to profiles attribute

--- client/templates/pages/myprofilepage.js
+++ client/templates/pages/myprofilepage.js
@@ -1,3 +1,4 @@
+
 Template.myProfile.helpers({
 
   profiles: function() {
@@ -5,15 +6,19 @@ Template.myProfile.helpers({
   }
 });
 
+var imageURL;
+
 Template.myProfile.events({
 
   'submit form': function(e) {
+
     e.preventDefault();
     var profile = {
       firstname: $(e.target).find('[name=firstname]').val(),
       lastname: $(e.target).find('[name=lastname]').val(),
       company: $(e.target).find('[name=company]').val(),
-      email: $(e.target).find('[name=email]').val()
+      email: $(e.target).find('[name=email]').val(),
+      imageurl: imageURL
       //shortdescription: $(e.target).find('[name=shortdescription]').val()
     };
 /*
@@ -33,13 +38,13 @@ Template.myProfile.events({
        //console.log(profile.lastname);
        //console.log(profile.company);
        //console.log(profile.email);
-        return throwError(error.reason); 
+        return throwError(error.reason);
        }
-       
+
        // show this result but route anyway
        if (result.profileExists)
          throwError('This link has already been posted'); //change this to update profile
-  
+
         });
       }
       if (result === true) {
@@ -52,9 +57,9 @@ Template.myProfile.events({
        //console.log(profile.company);
        //console.log(profile.email);
         //console.log(error.reason);
-         return throwError(error.reason); 
+         return throwError(error.reason);
        }
-       
+
        // show this result but route anyway
 
           });
@@ -71,6 +76,7 @@ Template.myProfile.events({
 				files:files,
 				path:"subfolder"
 			},function(e,r){
+        imageURL = r.url;
 				console.log(r);
 		});
 	}
